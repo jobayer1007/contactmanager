a11e5deaa6770bc0f05ab0c50a0e6dc002e7e069
jobayer1007 <62760527+jobayer1007@users.noreply.github.com>
2020-09-05 08:13:13 +0600

    Updates

---
+++ precache-manifest.44fabae293fc525a10bf70e321d173db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c660d10e1d6d45d3bd5cea2d967dfadd",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "c4a182fadc95deafa254",
+    "url": "/contactmanager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "83985d479e761fb17a67",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c4a182fadc95deafa254",
+    "url": "/contactmanager/static/js/2.70cd3128.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.70cd3128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83985d479e761fb17a67",
+    "url": "/contactmanager/static/js/main.e4362649.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
